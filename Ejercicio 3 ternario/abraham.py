be7b35d56748a6c098af11bc3e3516eaf132ff0d
abrahamcg <abraham.canovas.developer@gmail.com>
2024-11-21 12:23:08 +0100

    solucion ejercicio 3 abraham

---
+++ Ejercicio 3 ternario/abraham.py
@@ -0,0 +1,5 @@
+valor_input = int(input("Introduce un numero wacho"))
+
+valor_absoluto = valor_input if valor_input >= 0 else valor_input * -1
+
+print(f'El valor absoluto de {valor_input} es {valor_absoluto}')
