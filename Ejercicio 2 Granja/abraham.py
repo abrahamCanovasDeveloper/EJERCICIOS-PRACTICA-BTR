d2d2071397ae96cd3129854d6ca2f4e8b116e3f4
abrahamcg <abraham.canovas.developer@gmail.com>
2024-11-21 12:07:45 +0100

    corregir ejercicio 2 abraham.py

--- Ejercicio 2 Granja/abraham.py
+++ Ejercicio 2 Granja/abraham.py
@@ -1,9 +1,12 @@
-comida_diaria = 0
+comida_diaria = int(input("¿Cuantos kilos de comida compras al dia?"))
+num_animales = int(input("¿Cuantos animales hay en la granja?"))
+kilos_por_animal = int(input("¿Cuantos kilos come cada bestia al dia?"))
 
-num_animales = input("¿Cuantos animales hay en la granja?")
-kilos_por_animal = input("¿Cuantos kilos come cada bestia al dia?")
 
+comida_diaria_necesaria = kilos_por_animal * num_animales
 
-comida_diaria = kilos_por_animal * num_animales
-
-print(f'Necesitas {comida_diaria} al dia')
+if comida_diaria >= comida_diaria_necesaria:
+    print('Tenemos comida suficiente')
+else:
+    racion = comida_diaria / num_animales
+    print(f'No tenemos comida suficiente. Con lo que tenemos cada animal toca a {racion} kg')
